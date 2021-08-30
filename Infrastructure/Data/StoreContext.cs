a57cfd12a0140590ef8d97abcf5d1316fa601971
adiyy2001 <adiy@onet.pl>
2021-08-30 11:18:17 +0200

    add basic api got products

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
+using System.Reflection;
 
 namespace Infrastructure.Data
 {
@@ -14,5 +15,12 @@ namespace Infrastructure.Data
         }
 
         public DbSet<Product> Products {  get; set; }
+        public DbSet<ProductBrand> ProductBrands {  get; set; }
+        public DbSet<ProductType> ProductTypes {  get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
--- shop-be/Startup.cs
+++ shop-be/Startup.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Interfaces;
 
 namespace shop_be
 {
@@ -27,6 +28,7 @@ namespace shop_be
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
             services.AddDbContext<StoreContext>(
                 x => x.UseSqlite(_config.GetConnectionString("DefaultConnection"))
